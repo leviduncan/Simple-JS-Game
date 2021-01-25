78ea54d110800449068b2b00fed9dc1025776788
Darrin Duncan <darrin@darrinduncan.com>
2021-01-25 09:36:48 -0500

    Added hit detection

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ let character = document.getElementById("character")
 let block = document.getElementById("block")
 
 function jump() {
-    character.classList.add("animate")
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
     setTimeout(function(){
         character.classList.remove("animate");
     },500)
 }
+
+// hit detection
+let hitCheck = setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    let blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft < 0 && characterTop >= 130) {
+        block.style.aniimate = "none"
+        block.style.display = "none";
+      alert("You're Dead");
+    }
+},10)
+
+
+
