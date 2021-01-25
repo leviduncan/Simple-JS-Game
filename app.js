59e47669f1dbf019b610940636e3dde3fb56202d
Darrin Duncan <darrin@darrinduncan.com>
2021-01-24 23:58:00 -0500

    Simple JSGame using Animation and setTimout

---
+++ app.js
@@ -0,0 +1,9 @@
+let character = document.getElementById("character")
+let block = document.getElementById("block")
+
+function jump() {
+    character.classList.add("animate")
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500)
+}
